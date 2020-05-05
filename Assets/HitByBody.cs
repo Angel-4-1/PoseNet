88d387daa50babbbaa8b64f2e85770b2c028d177
Angel-4-1 <angel.vicente01@estudiant.upf.edu>
2020-05-05 17:44:44 +0200

    Interaction with both hands + nose

---
+++ Assets/HitByBody.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitByBody : MonoBehaviour
+{
+    private bool isHit;
+    private Rigidbody myRigidbody;
+    //public GameObject part;
+    private Collider myCollider;
+    public string body_part;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+        isHit = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(body_part) && !isHit)
+        {
+            isHit = true;
+            Destroy(gameObject);
+        }
+    }
+}
